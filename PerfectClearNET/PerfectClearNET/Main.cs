b57aa2189aed121054911538285ac1f7fc08ae3b
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-05-01 18:42:32 +0200

    Implement Find

--- PerfectClearNET/PerfectClearNET/Main.cs
+++ PerfectClearNET/PerfectClearNET/Main.cs
@@ -7,16 +7,41 @@ using System.Text;
 namespace PerfectClearNET {
     static class Interface {
         [DllImport("sfinder.dll")]
-        private static extern void action(StringBuilder str, int len);
+        private static extern void action(string field, string queue, string hold, StringBuilder str, int len);
 
-        public static string process() {
+        public static string Process(string field, string queue, string hold) {
             StringBuilder sb = new StringBuilder(500);
-            action(sb, sb.Capacity);
+            action(field, queue, hold, sb, sb.Capacity);
             return sb.ToString();
         }
     }
 
     public static class PerfectClear {
+        private static readonly string[] MinoMap = new string[7] {
+            "Z", "S", "L", "J", "T", "O", "I"
+        };
+
         static PerfectClear() {}
+
+        public static void Find(int[,] field, int[] queue, int current, int? hold) {
+            string f = "";
+
+            for (int i = 0; i < 24; i++)
+                for (int j = 0; j < 10; j++)
+                    f += (field[j,i] == 255)? '_' : 'X';
+
+            string q = MinoMap[current];
+
+            for (int i = 0; i < queue.Length; i++)
+                q += MinoMap[queue[i]];
+
+            string h = (hold == null) ? "E" : MinoMap[hold.Value];
+            
+            string result = Interface.Process(f, q, h);
+
+            if (!result.Equals("-1")) {
+
+            }
+        }
     }
 }
