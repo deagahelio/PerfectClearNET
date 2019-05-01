8b95d2aa7f2df3c90475e9bb5f388d3d1d231187
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-05-01 17:19:13 +0200

    Create PerfectClearNET C# project

---
+++ PerfectClearNET/PerfectClearNET/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PerfectClearNET {
+    static class Interface {
+        [DllImport("sfinder.dll")]
+        private static extern void action(StringBuilder str, int len);
+
+        public static string process() {
+            StringBuilder sb = new StringBuilder(500);
+            action(sb, sb.Capacity);
+            return sb.ToString();
+        }
+    }
+
+    public static class PerfectClear {
+        static PerfectClear() {}
+    }
+}
